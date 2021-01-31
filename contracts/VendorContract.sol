dd6506a347d166c758294094fb03f0674883e0a7
Gawain B. II <bracy.gawain2014@gmail.com>
2021-01-30 19:10:03 -0500

    Update VendorContract.sol

--- contracts/VendorContract.sol
+++ contracts/VendorContract.sol
@@ -1,20 +1,30 @@
+// "SPDX-License-Identifier: MIT"
 pragma solidity >=0.6.0 <0.7.0;
 
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/IERC20.sol";
 
 contract VendorContract {
 
-  IERC20 token;
+  IERC20 crtvToken;
 
-  constructor (address tokenAddress) public {
-    token = IERC20(tokenAddress);
+  constructor (IERC20 theToken) public {
+    crtvToken = theToken;
   }
 
   function buyToken() public payable {
-    require(token.transfer(msg.sender, msg.value) );
+    require(crtvToken.transfer(msg.sender, msg.value), "TRANSFER FAILED" );
   }
 
-  //function sellToken();
+  function sellToken(uint256 amount) public {
+    require(crtvToken.transferFrom(msg.sender, address(this), amount), "TRANSFER FAILED");
+    (bool success, ) = msg.sender.call{value: amount}("");
+    (!success); 
+    
+    
+  }
+
+  receive() external payable {
+    buyToken();
+  }
 
 }

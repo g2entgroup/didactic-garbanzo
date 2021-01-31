24550c9bb5092c21e9193456801b24cdb3bed877
Gawain B. II <bracy.gawain2014@gmail.com>
2021-01-30 19:06:29 -0500

    Update CrtvTokenContract.sol

--- contracts/CrtvTokenContract.sol
+++ contracts/CrtvTokenContract.sol
@@ -1,13 +1,11 @@
+// "SPDX-License-Identifier: MIT"
 pragma solidity >=0.6.0 <0.7.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract CrtvTokenContract is ERC20 {
-
-  constructor () ERC20("CreativeToken","CRTV") public {
-    // what should we do on deploy?
-    _mint(msg.sender, 980000000 ether);
+contract CRTVToken is ERC20 {
+  constructor() ERC20("Creative Token","CRTV") public {
+      _mint(msg.sender, 980 * 10 ** uint(decimals()));
   }
-
 }

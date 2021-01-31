9e5f43ab2563be6c62800ab0e3ab10afb9578c6f
Gawain B. II <bracy.gawain2014@gmail.com>
2021-01-30 19:12:27 -0500

    Delete CRTVSwap.sol

--- contracts/CRTVSwap.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.6.0 <0.7.0;
-
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-
-contract CRTVSwap is ERC20 {
-  constructor() ERC20("Creative Tokens","CRTV") public {
-      _mint(msg.sender,1000*10**18);
-  }
-}

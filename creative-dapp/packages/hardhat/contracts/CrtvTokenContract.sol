448c59fc1516eb9660f37f0a8e99bf51359afc6a
Gawain B. II <bracy.gawain2014@gmail.com>
2021-02-02 10:56:31 -0500

    Update CrtvTokenContract.sol

--- creative-dapp/packages/hardhat/contracts/CrtvTokenContract.sol
+++ creative-dapp/packages/hardhat/contracts/CrtvTokenContract.sol
@@ -5,6 +5,9 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract CrtvTokenContract is ERC20 {
+
+  event SetPurpose(address sender, string purpose);
+ 
   constructor() ERC20("Creative Token","CRTV") public {
       _mint(msg.sender, 980000000 * 10 ** uint(decimals()));
   }
